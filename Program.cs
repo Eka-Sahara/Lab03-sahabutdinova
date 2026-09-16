4992088404f7b4dc15eeb2cef846e2903f7a962c
Eka-Sahara <ekaterinasahabutdinova473@gmail.com>
2026-09-16 15:21:57 +0300

    Lab03: increment and decrement

--- Program.cs
+++ Program.cs
@@ -14,3 +14,21 @@ System.Console.WriteLine($"После начисление 5%: {balance}");
 
 balance /= 2; // разделили счёт пополам с партнёром
 System.Console.WriteLine($"После деления пополам: {balance}");
+
+
+System.Console.WriteLine();
+System.Console.WriteLine("Операторы сравнения");
+
+double myGrade = 4.6;
+double passingGrade = 4.0;
+int myAge = 20;
+int votingAge = 18;
+bool isPassing = myGrade >= passingGrade;
+bool isExactAge = myAge == votingAge;
+bool canVote = myAge >= votingAge;
+bool isNotFailing = myGrade != 2.0;
+
+System.Console.WriteLine($"Балл: {myGrade} >= {passingGrade}: {isPassing}");
+System.Console.WriteLine($"Возраст: {myAge} == {votingAge}: {isExactAge}");
+System.Console.WriteLine($"Возраст: {myAge} >= {votingAge} (может голосовать): {canVote}");
+System.Console.WriteLine($"Балл {myGrade} != 2.0 (не двойка): {isNotFailing}");
